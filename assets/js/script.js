e00f12a9ebb4b5c4b4bac85dc24f67fbb93c0665
LichKing112 <yehezkieldio35@gmail.com>
2020-06-05 12:47:45 +0800

    Intial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+  var typed2 = new Typed('#word', {
+    strings: ['Moderation Commands', 'Entertaiment Commands', 'Adminstration Commands'],
+    typeSpeed: 50,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: true
+  });
+    
