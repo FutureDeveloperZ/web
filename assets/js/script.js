cf215fbc9b93df0c3c93780dfd39292a40cc6429
Yehezkiel <47420407+LichKing112@users.noreply.github.com>
2020-08-24 20:24:21 +0800

    first fix on broken site

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-  var typed2 = new Typed('#word', {
-    strings: ['Moderation Commands', 'Entertaiment Commands', 'Adminstration Commands'],
-    typeSpeed: 50,
-    backSpeed: 0,
-    fadeOut: true,
-    loop: true
-  });
-    
